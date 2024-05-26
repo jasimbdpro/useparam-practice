896177e78848b371a3d14c0b36e868d3985095ca
Jasim Uddin <jasimbdpro@gmail.com>
2024-05-26 22:24:53 +0600

    'ne'

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <br />
-            <Link to="/about">About</Link>
+        <nav className='Navbar'>
+            <Link className='nav2' to="/">Home</Link>
+            <Link className='nav2' to="/about">About</Link>
         </nav>
     );
 };
